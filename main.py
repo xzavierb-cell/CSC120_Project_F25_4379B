30d3e037b351070a60ce3330968e2184aec43d8b
Xzavierb <xzvierb20@gmail.com>
2025-10-01 15:17:03 -0400

    main.py2

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ print("================================")
 print("Before we begin, what is your characters's name?")
 
 c_name = input()
-
 print("Great,", c_name + "!", "Let's begin the adventure!")
 
 player = {
@@ -12,3 +11,15 @@ player = {
     "health": 100,
     "coin": 0,
 }
+import random
+
+events = ["find a coin", "meet a monster", "do nothiing"]
+event = random.choice(events)
+print(f"While exploring, you {event}!")
+
+player = {
+    "name": c_name,
+    "coin": 5
+}
+player["coin"] += 1
+print(f"Player", c_name, "found a coin", c_name, "now has" {player["coin"]} "coins.")
