1f4dfc40b8245833acfd85e204575d9d52bb95d8
Xzavierb <xzvierb20@gmail.com>
2025-10-01 14:01:53 -0400

    main.py1

---
+++ main.py
@@ -0,0 +1,14 @@
+from logging import config
+
+
+game_name = "Xetra"
+print("Greetings Welcome to", game_name)
+print("====================")
+
+print("Before we begin, what is your character's name?")
+c_name = input()
+print("Great", c_name +"!","Let's begin the adventure!" )
+
+
+git ,config --global user.name "Xzavier Brown"
+git ,config --global user.email "xzavierb20@gmia.com"
